401d6d7c5ab2333c0ddc41518005d61875fb2283
MCyssa <mariacecilia.mvh@gmail.com>
2025-04-03 13:21:46 -0300

    Alterações

---
+++ script.js
@@ -0,0 +1,27 @@
+const textos = ["Front-end", "Criativa", "Apaixonada por código"];
+let index = 0;
+let letraIndex = 0;
+const typingElement = document.getElementById('typing');
+
+function digitar() {
+    if (letraIndex < textos[index].length) {
+        typingElement.textContent += textos[index][letraIndex];
+        letraIndex++;
+        setTimeout(digitar, 100);
+    } else {
+        setTimeout(apagar, 500)
+    }
+};
+
+function apagar() {
+    if (letraIndex > 0) {
+        typingElement.textContent = textos[index].substring(0, letraIndex - 1);
+        letraIndex--;
+        setTimeout(apagar, 100);
+    } else {
+        index = (index + 1) % textos.length;
+        setTimeout(digitar, 500);
+    }
+}
+
+digitar();
