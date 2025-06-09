2be064da3521dcf7ca7d9946728f23675335a0d7
MCyssa <mariacecilia.mvh@gmail.com>
2025-06-09 16:14:58 -0300

    Adicionado menu responsivo

--- script.js
+++ script.js
@@ -25,3 +25,10 @@ function apagar() {
 }
 
 digitar();
+
+const menuToggle = document.getElementById('menu-toggle');
+const navbar = document.getElementById('navbar');
+
+menuToggle.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
